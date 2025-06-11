22d1b4cc023f95a6fc38df62595d2e0113389511
jovidon <xalilovjovidon777@gmail.com>
2025-06-11 16:47:43 +0500

    to'g'irlandi

--- lib/core/data/client.dart
+++ lib/core/data/client.dart
@@ -1,10 +1,10 @@
 import 'package:dio/dio.dart';
 
 class ApiClient {
-  final Dio dio = Dio(BaseOptions(baseUrl: "https://fakestoreapi.com/docs"));
+  final Dio dio = Dio(BaseOptions(baseUrl: "https://fakestoreapi.com"));
 
   Future<List<dynamic>> fetchProduct() async {
-    final response = await dio.get('product');
+    final response = await dio.get('/products');
     if (response.statusCode == 200 ) {
       final List<dynamic> products = List.from(response.data);
       return products;
